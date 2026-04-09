b834464b5ae571c55c08cffa1d88f816e8debbd5
remoschenk007 <remoschenk@gmail.com>
2026-04-09 11:42:33 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -7,7 +7,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const SUPABASE_URL = 'https://pnynkzrqnfoshojqfqxn.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_T8XzRIhQymVgGn2sqZPdpA_LK0a_iyH'
+const SUPABASE_KEY = const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBueW5renJxbmZvc2hvanFmcXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MTg3NDEsImV4cCI6MjA5MTI5NDc0MX0.W3cOPU7lQKimHIYPc7ISuZGmOeV20GB3DEW-QdDJXZQ'
+
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
