52fd0672144e7f7c18313ceb6a4dada173a6f8c3
remoschenk007 <remoschenk@gmail.com>
2026-04-10 08:52:55 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -187,6 +187,8 @@ document.addEventListener('DOMContentLoaded', () => {
 // ============================================
 
 async function loadLocations() {
+   alert('loadLocations startet');
+
   const SUPABASE_URL = 'https://pnynkzrqnfoshojqfqxn.supabase.co'
   const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBueW5renJxbmZvc2hvanFmcXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MTg3NDEsImV4cCI6MjA5MTI5NDc0MX0.W3cOPU7lQKimHIYPc7ISuZGmOeV20GB3DEW-QdDJXZQ'
 
