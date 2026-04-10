c5811899285fdf959bbf2a0569e0e62dc7f90637
remoschenk007 <remoschenk@gmail.com>
2026-04-10 09:19:57 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -204,6 +204,8 @@ SUPABASE_URL + '/rest/v1/locations?select=*',
   )
 
   const data = await res.json()
+alert('Daten: ' + data.length + ' Locations')
+
   const container = document.getElementById('supabase-locations')
   if (!container) return
 
