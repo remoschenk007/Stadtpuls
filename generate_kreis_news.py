7ce6424298f52c8539d642295f90a8043e31920a
Alessandra Christen <alessandrachristen@Mac.fritz.box>
2026-07-23 12:29:48 +0200

    Kreis-Hubs: Header/Menu-Links fixe (base href)

--- generate_kreis_news.py
+++ generate_kreis_news.py
@@ -132,6 +132,7 @@ def page(k):
 PAGE_HTML = r"""<!DOCTYPE html>
 <html lang="de-CH">
 <head>
+<base href="/">
 <meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>__TITLE__</title>
 <meta name="description" content="__META__">
